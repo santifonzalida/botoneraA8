904acc9b7a8722e827944cafeb43c9676fe391a7
santiago <santiifonzalida@gmail.com>
2017-09-28 13:12:18 -0300

    sonidos nuevos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,19 +10,25 @@ export class HomeComponent implements OnInit {
   public filaAudio1: any[] = [];
 
   constructor() {
-
+    filaAudio1 = [
+      {id: 1, src= '../assets/audio/RonnieColeman.mp3'},
+      {id: 2, src= '../assets/audio/enduEndu.mp3'},
+      {id: 3, src= '../assets/audio/CarreraAparentemente.mp3'},
+      {id: 4, src= '../assets/audio/uaaahah.mp3'}
+    ];
   }
 
   ngOnInit() { }
 
-
-  public play(id: number) {
-    console.log("yeah sound!");
-    this.audio.src = "../assets/audio/RonnieColeman.mp3";
-    this.audio.load();
+  public play(index: number) {
+    for (let sonido of fileAudio1) {
+      if(sonido.id == index){
+        this.audio.src = sonido.src;
+        this.audio.load();
+      }
+    }
     this.audio.play();
   }
-
 
   public stop() {
     this.audio.pause();
