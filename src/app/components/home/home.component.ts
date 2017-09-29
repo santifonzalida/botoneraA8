5a0c2781776c080864ecb24c79b7b8bd7447b5bc
Dario Diaz <darioo.diaz1@gmail.com>
2017-09-29 15:04:24 -0300

    nuevo sonido gg

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,33 +14,16 @@ export class HomeComponent implements OnInit {
     'carrera': 'CarreraAparentemente',
     'ua': 'uaaahah',
     'endu': 'enduEndu',
-	'tramboliko': 'tramboliko',
-	'volo':'volo'
+    'tramboliko': 'tramboliko',
+    'volo': 'volo',
+    'gg': 'gg'
   };
-
-  constructor() {
-    /*filaAudio1 = [
-      {id: 1, src= '../assets/audio/RonnieColeman.mp3'},
-      {id: 2, src= '../assets/audio/enduEndu.mp3'},
-      {id: 3, src= '../assets/audio/CarreraAparentemente.mp3'},
-      {id: 4, src= '../assets/audio/uaaahah.mp3'}
-    ];*/
-  }
-
   ngOnInit() { }
-  
+
   public play(sound) {
     console.log("yeah sound!");
     this.audio.src = `../assets/audio/${this.SOUNDS[sound]}.mp3`;
     this.audio.load();
-    
-  /*public play(index: number) {
-    for (let sonido of fileAudio1) {
-      if(sonido.id == index){
-        this.audio.src = sonido.src;
-        this.audio.load();
-      }
-    }*/
     this.audio.play();
   }
 
