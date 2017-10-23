9c0dbf4b101455959545a785415fed80d86ac1d4
Santiago <santiifonzalida@gmail.com>
2017-10-23 11:33:54 -0300

    Update home.component.ts
    update version name

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeComponent {
 
-  version: string = 'v1.0';
+  version: string = 'v1.078';
   audio = new Audio();
 
   private SOUNDS = {
